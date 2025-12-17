954ff527aabc6bf6275e2380b4db405f52f56466
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2025-12-17 15:42:33 +0500

    -i class-validatro

--- bad-fockarch/src/bad-fockarch/files/ts/dto/__name__-create.dto.ts
+++ bad-fockarch/src/bad-fockarch/files/ts/dto/__name__-create.dto.ts
@@ -1,6 +1,7 @@
 import type { <%= classify(name) %> } from "v1/types/<%= name %>.types";
 
 import { ApiProperty } from "@nestjs/swagger";
+import {  } from 'class-validator';
 
 export class <%= classify(name) %>CreateDto implements Partial<<%= classify(name) %>> {
 
--- bad-fockarch/src/bad-fockarch/files/ts/dto/__name__-update.dto.ts
+++ bad-fockarch/src/bad-fockarch/files/ts/dto/__name__-update.dto.ts
@@ -1,6 +1,7 @@
 import type { <%= classify(name) %> } from "v1/types/<%= name %>.types";
 
 import { ApiProperty } from "@nestjs/swagger";
+import {  } from 'class-validator';
 
 export class <%= classify(name) %>UpdateDto implements Partial<<%= classify(name) %>> {
 
