1eaa5c81cf40b75e8a8a8b7d10601957f00ca57a
FOCKUSTY <wwddwwwd@mail.ru>
2026-02-13 18:05:43 +0500

    -del errors.md registring

--- src/v1/errors/index.ts
+++ src/v1/errors/index.ts
@@ -1,8 +1,4 @@
-import ErrorConstructor from "./constructor";
-
 export * from "./guards";
 export * from "./pipes";
 export * from "./services";
 export * from "./strategies";
-
-ErrorConstructor.close();
