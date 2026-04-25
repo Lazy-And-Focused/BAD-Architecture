5b033976484d65935151c74db3d76919f2aab0fe
FOCKUSTY <wwddwwwd@mail.ru>
2026-04-25 12:43:15 +0500

    refactor: third iteractoion, utils

--- bad-fockarch/src/utils/name.parser.ts
+++ bad-fockarch/src/utils/name.parser.ts
@@ -1,5 +1,5 @@
 import type { Path } from "@angular-devkit/core";
-import { basename, dirname, normalize } from "@angular-devkit/core";
+import { basename, dirname, join, normalize } from "@angular-devkit/core";
 
 export interface ParseOptions {
   name: string;
@@ -15,13 +15,12 @@ export class NameParser {
   public parse(options: ParseOptions): Location {
     const nameWithoutPath: string = basename(options.name as Path);
     const namePath: string = dirname(
-      (options.path === undefined ? "" : options.path)
-        .concat("/")
-        .concat(options.name) as Path,
+      join((options.path ?? "") as Path, options.name)
     );
+    
     return {
       name: nameWithoutPath,
-      path: normalize("/".concat(namePath)),
+      path: normalize(join("/" as Path, namePath)),
     };
   }
 }
